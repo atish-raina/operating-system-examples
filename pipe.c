b8e5669009839f5ca82dc8bbe2e48d266ecf9983
atish <atishlearn@gmail.com>
2025-11-03 11:23:07 +0530

    add the init c program

---
+++ pipe.c
@@ -0,0 +1,12 @@
+#define _XOPEN_SOURCE 700
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+int main(void) {
+	int fds[2];
+}
